798db603dd4f0322ddce51cca7ef959b7539265d
shehzadfalcon <shehzad.falcon@gmail.com>
2022-01-31 22:40:30 +0500

    path changeds

--- utils/generatePdf.js
+++ utils/generatePdf.js
@@ -4,9 +4,8 @@ const optionsPDF = { width: 1024, height: 768 };
 
 exports.GeneratePdf = async (html = "") => {
   const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    args: ["--no-sandbox"],
     ignoreDefaultArgs: ["--disable-extensions"],
-    // executablePath: "/usr/bin/chromium-browser",
   });
   const page = await browser.newPage();
   await page.setContent(html, {
