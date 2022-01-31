803513835997e21d877b356c98a624b2531610a1
shehzadfalcon <shehzad.falcon@gmail.com>
2022-01-31 23:07:25 +0500

    pupetter installedss:false

--- utils/generatePdf.js
+++ utils/generatePdf.js
@@ -4,7 +4,7 @@ const optionsPDF = { width: 1024, height: 768 };
 
 exports.GeneratePdf = async (html = "") => {
   const browser = await puppeteer.launch({
-    args: ["--no-sandbox"],
+    // args: ["--no-sandbox"],
     // ignoreDefaultArgs: ["--disable-extensions"],
     // executablePath: "/usr/bin/chromium-browser",
     headless: false,
